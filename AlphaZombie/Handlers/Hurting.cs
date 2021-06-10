b3ef859eecce56874d82f0541c1a0f85ac9f602b
Will Morris <teamexangus@gmail.com>
2021-06-10 12:25:36 +1000

    Split up some functions

--- AlphaZombie/Handlers/Hurting.cs
+++ AlphaZombie/Handlers/Hurting.cs
@@ -10,30 +10,42 @@ namespace AlphaZombie.Handlers
             //Inflicts status effects and damage, and sends broadcast, when player is hit by Alpha Zombie
             if (ev.Target != ev.Attacker && Functions.IsAlphaZombie(ev.Attacker))
             {
-                if (AlphaZombie.Instance.Config.BroadcastWhenHit != "none")
-                {
-                    ev.Target.Broadcast(AlphaZombie.Instance.Config.BroadcastDuration, AlphaZombie.Instance.Config.BroadcastWhenHit);
-                }
-
-                ev.Amount = AlphaZombie.Instance.Config.AlphaZombieAttackDamage;
-                foreach (EffectType effect in AlphaZombie.Instance.Config.AlphaZombieInflict)
-                {
-                    ev.Target.EnableEffect(effect);
-                }
+                HitByAlphaZombie(ev);
             }
 
             //Alpha Zombie Damage Handlers
             if (Functions.IsAlphaZombie(ev.Target))
             {
-                //This would look better with a switch statement: Too bad
-                if (ev.DamageType == DamageTypes.Scp207)
-                {
-                    ev.IsAllowed = false;
-                }
-                else if (ev.DamageType == DamageTypes.Decont)
-                {
-                    ev.Amount = AlphaZombie.Instance.Config.AlphaZombieDecontDamage;
-                }
+                AlphaZombieDamageHandlers(ev);
+            }
+        }
+
+        //Called when a player is hit by Alpha Zombie, sends broadcast and inflicts status effects
+        public void HitByAlphaZombie(HurtingEventArgs ev)
+        {
+            if (AlphaZombie.Instance.Config.BroadcastWhenHit != "none")
+            {
+                ev.Target.Broadcast(AlphaZombie.Instance.Config.BroadcastDuration, AlphaZombie.Instance.Config.BroadcastWhenHit);
+            }
+
+            ev.Amount = AlphaZombie.Instance.Config.AlphaZombieAttackDamage;
+            foreach (EffectType effect in AlphaZombie.Instance.Config.AlphaZombieInflict)
+            {
+                ev.Target.EnableEffect(effect);
+            }
+        }
+
+        //Called when Alpha Zombie takes damage, prevents coke damage and modifies decont damage
+        public void AlphaZombieDamageHandlers(HurtingEventArgs ev)
+        {
+            //This would look better with a switch statement: Too bad
+            if (ev.DamageType == DamageTypes.Scp207)
+            {
+                ev.IsAllowed = false;
+            }
+            else if (ev.DamageType == DamageTypes.Decont)
+            {
+                ev.Amount = AlphaZombie.Instance.Config.AlphaZombieDecontDamage;
             }
         }
     }
--- AlphaZombie/Handlers/Spawning.cs
+++ AlphaZombie/Handlers/Spawning.cs
@@ -18,31 +18,42 @@ namespace AlphaZombie.Handlers
             //Spawning Alpha Zombie with SCP-049-2
             if (ev.Player.Role == RoleType.Scp049 && Round.ElapsedTime.TotalSeconds <= 5) //Five seconds chosen arbitrarily
             {
-                var playerList = ListPool<Player>.Shared.Rent(Player.List); //Definitely not ripped from Stalky106
-                int playerCount = playerList.Count;
-
-                //If enough players online && random chance based on configs
-                if (playerCount >= AlphaZombie.Instance.Config.MinPlayersForSpawn && AlphaZombie.Instance.random.Next(0, 100) <= AlphaZombie.Instance.Config.AlphaZombieSpawnChance)
-                {
-                    Player newAlphaZombie = playerList[AlphaZombie.Instance.random.Next(0, playerCount)];
-                    int tries = 0;
-
-                    //Over-engineered code segment prevents Alpha Zombie from replacing the SCP-049 that spawned it
-                    while (newAlphaZombie.Role == RoleType.Scp049)
-                    {
-                        newAlphaZombie = playerList[AlphaZombie.Instance.random.Next(0, playerCount)];
-
-                        //Will prevent the server from hanging in the event that only SCP-049s exist
-                        if (tries++ > 5) { return; } //Five loops is chosen aribtrarily, perhaps I was thinking about five a lot when I wrote this code
-                    }
-
-                    //Players are spawned one-by-one, so CallDelayed() prevents players from being set to Alpha Zombie then back to a normal class
-                    Timing.CallDelayed(1f, () =>
-                    {
-                        Functions.SpawnAlphaZombie(newAlphaZombie);
-                    });
-                }
+                var spawnSuccess = SpawningAZ();
+                Log.Debug("Failed to spawn Alpha Zombie.", !(spawnSuccess) && AlphaZombie.Instance.Config.DebugMessages);
             }
+        }
+
+        //Boolean return value states whether a player was spawned or not.
+        public bool SpawningAZ()
+        {
+            var playerList = ListPool<Player>.Shared.Rent(Player.List); //Definitely not ripped from Stalky106
+            int playerCount = playerList.Count;
+
+            //If enough players online && random chance based on configs
+            if (playerCount >= AlphaZombie.Instance.Config.MinPlayersForSpawn && AlphaZombie.Instance.random.Next(0, 100) <= AlphaZombie.Instance.Config.AlphaZombieSpawnChance)
+            {
+                Player newAlphaZombie = playerList[AlphaZombie.Instance.random.Next(0, playerCount)];
+                int tries = 0;
+
+                //Over-engineered code segment prevents Alpha Zombie from replacing the SCP-049 that spawned it
+                while (newAlphaZombie.Role == RoleType.Scp049)
+                {
+                    newAlphaZombie = playerList[AlphaZombie.Instance.random.Next(0, playerCount)];
+
+                    //Will prevent the server from hanging in the event that only SCP-049s exist
+                    if (tries++ > 5) { return false; } //Five loops is chosen aribtrarily, perhaps I was thinking about five a lot when I wrote this code
+                }
+
+                //Players are spawned one-by-one, so CallDelayed() prevents players from being set to Alpha Zombie then back to a normal class
+                Timing.CallDelayed(1f, () =>
+                {
+                    Functions.SpawnAlphaZombie(newAlphaZombie);
+                });
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
